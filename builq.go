e24beefb0f24f2473ed09690592b148762721874
junk1tm <github@junk1tm.me>
2022-11-08 17:09:23 +0400

    feat: check for different placeholders

--- builq.go
+++ builq.go
@@ -1,21 +1,32 @@
 package builq
 
 import (
+	"errors"
 	"fmt"
 	"strings"
 )
 
+// ErrDifferentPlaceholders is returned by [Builder.Build] when different
+// placeholders are used in a single query (e.g. WHERE foo = %$ AND bar = %?).
+var ErrDifferentPlaceholders = errors.New("builq: different placeholders must not be used")
+
 // Builder for SQL queries.
 type Builder struct {
-	query   strings.Builder
-	args    []any
-	counter int   // a counter for numbered placeholders ($1, $2, ...).
-	err     error // the first error occurred while building the query.
+	query        strings.Builder
+	args         []any
+	counter      int               // a counter for numbered placeholders ($1, $2, ...).
+	err          error             // the first error occurred while building the query.
+	placeholders map[rune]struct{} // a set of placeholders used to build the query.
 }
 
 func (b *Builder) Appendf(format string, args ...any) *Builder {
+	if b.placeholders == nil {
+		b.placeholders = make(map[rune]struct{})
+	}
+
+	// return earlier if there is already an error.
 	if b.err != nil {
-		return b // return earlier if there is already an error.
+		return b
 	}
 
 	wargs := make([]any, len(args))
@@ -30,6 +41,9 @@ func (b *Builder) Appendf(format string, args ...any) *Builder {
 }
 
 func (b *Builder) Build() (string, []any, error) {
+	if len(b.placeholders) > 1 {
+		return "", nil, ErrDifferentPlaceholders
+	}
 	return b.query.String(), b.args, b.err
 }
 
@@ -47,11 +61,13 @@ func (a *argument) Format(s fmt.State, v rune) {
 
 	case '$': // PostgreSQL
 		a.builder.args = append(a.builder.args, a.value)
+		a.builder.placeholders[v] = struct{}{}
 		a.builder.counter++
 		fmt.Fprintf(s, "$%d", a.builder.counter)
 
 	case '?': // MySQL/SQLite
 		a.builder.args = append(a.builder.args, a.value)
+		a.builder.placeholders[v] = struct{}{}
 		fmt.Fprint(s, "?")
 
 	default:
