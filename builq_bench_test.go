93a9f637d07caabcfc131556e048f0caeee78dbe
Oleg Kovalov <oleg@hey.com>
2024-03-11 21:15:42 +0100

    Update benchmark syntax

--- builq_bench_test.go
+++ builq_bench_test.go
@@ -8,10 +8,10 @@ import (
 
 func BenchmarkBuildSimple(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		var bb builq.Builder
-		bb.Addf("SELECT %s FROM %s", "foo,bar", "table")
-		bb.Addf("WHERE id = %$", 123)
-		query, args, err := bb.Build()
+		q := builq.New()
+		q("SELECT %s FROM %s", "foo,bar", "table")
+		q("WHERE id = %$", 123)
+		query, args, err := q.Build()
 
 		switch {
 		case err != nil:
@@ -26,16 +26,16 @@ func BenchmarkBuildSimple(b *testing.B) {
 
 func BenchmarkBuildManyArgs(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		var bb builq.Builder
-		bb.Addf("SELECT %s FROM %s", "foo,bar", "table")
-		bb.Addf("WHERE id = %$", 123)
-		bb.Addf("AND user_id = %$ OR referral_id = %$", 123, 456)
-		bb.Addf("OR status = %$ OR name = %$", "admin", "nice")
-		bb.Addf("AND password LIKE %$ OR password = %$", "str0ng", "qwerty")
-		bb.Addf("GROUP BY %$", "foo")
-		bb.Addf("ORDER BY %$", "bar")
-		bb.Addf("LIMIT %$;", 123)
-		query, args, err := bb.Build()
+		q := builq.New()
+		q("SELECT %s FROM %s", "foo,bar", "table")
+		q("WHERE id = %$", 123)
+		q("AND user_id = %$ OR referral_id = %$", 123, 456)
+		q("OR status = %$ OR name = %$", "admin", "nice")
+		q("AND password LIKE %$ OR password = %$", "str0ng", "qwerty")
+		q("GROUP BY %$", "foo")
+		q("ORDER BY %$", "bar")
+		q("LIMIT %$;", 123)
+		query, args, err := q.Build()
 
 		switch {
 		case err != nil:
@@ -49,18 +49,36 @@ func BenchmarkBuildManyArgs(b *testing.B) {
 }
 
 func BenchmarkBuildCached(b *testing.B) {
-	var bb builq.Builder
-	bb.Addf("SELECT %s FROM %s", "foo,bar", "table")
-	bb.Addf("WHERE id = %$", 123)
-	bb.Addf("AND user_id = %$ OR referral_id = %$", 123, 456)
-	bb.Addf("OR status = %$ OR name = %$", "admin", "nice")
-	bb.Addf("GROUP BY %$", "foo")
-	bb.Addf("ORDER BY %$", "bar")
-	bb.Addf("LIMIT %$;", 123)
-	query, args, err := bb.Build()
+	q := builq.New()
+	q("SELECT %s FROM %s", "foo,bar", "table")
+	q("WHERE id = %$", 123)
+	q("AND user_id = %$ OR referral_id = %$", 123, 456)
+	q("OR status = %$ OR name = %$", "admin", "nice")
+	q("GROUP BY %$", "foo")
+	q("ORDER BY %$", "bar")
+	q("LIMIT %$;", 123)
+	query, args, err := q.Build()
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
+		switch {
+		case err != nil:
+			b.Fatal(err)
+		case query == "":
+			b.Fatal("empty query")
+		case len(args) == 0:
+			b.Fatal("empty args")
+		}
+	}
+}
+
+func BenchmarkBuilder(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		var bb builq.Builder
+		bb.Addf("SELECT %s FROM %s", "foo,bar", "table")
+		bb.Addf("WHERE id = %$", 123)
+		query, args, err := bb.Build()
+
 		switch {
 		case err != nil:
 			b.Fatal(err)
